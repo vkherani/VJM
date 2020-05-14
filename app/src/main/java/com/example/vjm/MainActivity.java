f680b571bb36ef13f826c34df23eba40370cd42a
vkherani <vkherani@gmail.com>
2020-05-14 17:45:59 +0530

    design added by vasim

--- app/src/main/java/com/example/vjm/MainActivity.java
+++ app/src/main/java/com/example/vjm/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.vjm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.transition.Slide;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 //hello
+
+    ViewPager viewPager;
+    Handler sliderHandler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.viewPager);
+
+        final List<SliderItem> sliderItems = new ArrayList<>();
+        sliderItems.add(new SliderItem(R.drawable.first));
+        sliderItems.add(new SliderItem(R.drawable.second));
+        sliderItems.add(new SliderItem(R.drawable.third));
+        sliderItems.add(new SliderItem(R.drawable.fourth));
+
+        SliderAdapter adapter = new SliderAdapter(sliderItems,getApplicationContext(),viewPager);
+        viewPager.setAdapter(adapter);
+        viewPager.setPadding(130,0,130,0);
+        sliderHandler.removeCallbacks(sliderRunnable);
+        sliderHandler.postDelayed(sliderRunnable,3000);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable,3000);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
+
+    Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+        }
+    };
+
 }
---
+++ app/src/main/java/com/example/vjm/SliderAdapter.java
@@ -0,0 +1,73 @@
+package com.example.vjm;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.List;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class SliderAdapter extends PagerAdapter {
+
+    List<SliderItem> sliderItems;
+    Context context;
+    ViewPager viewPager;
+
+    public SliderAdapter(List<SliderItem> sliderItems, Context context,ViewPager viewPager) {
+        this.sliderItems = sliderItems;
+        this.context = context;
+        this.viewPager = viewPager;
+    }
+
+    @Override
+    public int getCount() {
+        return sliderItems.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        View view = LayoutInflater.from(context).inflate(R.layout.slider_item_container,container,false);
+
+        ImageView imageView;
+        imageView = view.findViewById(R.id.imageView);
+
+        imageView.setImageResource(sliderItems.get(position).getImage());
+        container.addView(view,0);
+        if(position == sliderItems.size() - 2)
+        {
+            viewPager.post(runnable);
+        }
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View)object);
+    }
+
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            sliderItems.addAll(sliderItems);
+            notifyDataSetChanged();
+        }
+    };
+}
+
+
