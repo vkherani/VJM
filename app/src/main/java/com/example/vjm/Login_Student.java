98047f5222fb7050b7c2651341807e9721daacfb
KunjKanani <kunjkanani27@gmail.com>
2020-05-15 14:56:03 +0530

    add login

---
+++ app/src/main/java/com/example/vjm/Login_Student.java
@@ -0,0 +1,14 @@
+package com.example.vjm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Student extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__student);
+    }
+}
