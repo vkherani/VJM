aec67474ccb14ce5fbac31a160f868383db3ae7a
vkherani <vkherani@gmail.com>
2020-05-16 16:30:25 +0530

    carousel complete

--- app/src/main/java/com/example/vjm/SliderAdapter.java
+++ app/src/main/java/com/example/vjm/SliderAdapter.java
@@ -1,12 +1,14 @@
 package com.example.vjm;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
@@ -37,6 +39,7 @@ public class SliderAdapter extends PagerAdapter {
         return view.equals(object);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
