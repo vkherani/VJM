67ca08428b93af15f544f2889edcd7e244c1cca6
KunjKanani <kunjkanani27@gmail.com>
2020-05-16 15:48:09 +0530

    design of homepage complete

--- app/src/main/java/com/example/vjm/student_pass.java
+++ app/src/main/java/com/example/vjm/student_pass.java
@@ -2,13 +2,29 @@ package com.example.vjm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class student_pass extends AppCompatActivity {
 
+    Button signupFinal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_pass);
+
+
+        signupFinal = findViewById(R.id.signupFinal);
+
+        signupFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), homepage_student.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
